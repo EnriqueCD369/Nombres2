be00132ca836bef352f55d3dbe9450c9e603930f
Enrique Chan <chandiazenrique@gmail.com>
2024-02-28 18:28:47 -0600

    Se agrego una variable de cadena

--- PruebasGit.java
+++ PruebasGit.java
@@ -3,6 +3,7 @@ package PorbandoImprimir;
 public class PruebasGit {
 	public static void main(String args[]) {
 	System.out.println("Probando Ramas En Git");
-	int numero;	
+	int numero;
+	String nombre;
 	}
 }
